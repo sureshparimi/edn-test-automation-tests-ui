d50deed1016353654b52892761c140f1f74031de
sureshparimi1 <suresh.parimi@pfizer.com>
2021-02-25 11:55:28 +0530

    path of features to be collected modified

--- tests-ui/step_definitions/test_edison_features.py
+++ tests-ui/step_definitions/test_edison_features.py
@@ -1,3 +1,3 @@
 from pytest_bdd import scenarios
 # set the path of the features or sub dir or specific feature file to run
-scenarios('../edison_test_automation_suite/tests-ui/features/changelogs')
+scenarios('../features/changelogs')
